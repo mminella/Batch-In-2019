25b3096e2f6ece33baeccc70d7d12764bd37aa05
Michael Minella <mminella@pivotal.io>
2019-02-27 11:28:30 -0600

    Initial commit

---
+++ batch-job/src/main/java/io/spring/batch/BatchIn2019Application.java
@@ -0,0 +1,13 @@
+package io.spring.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchIn2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchIn2019Application.class, args);
+	}
+
+}
---
+++ batch-job/src/main/java/io/spring/batch/configuration/BatchConfiguration.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.spring.batch.configuration;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Michael Minella
+ */
+@EnableTask
+@EnableBatchProcessing
+@Configuration
+public class BatchConfiguration {
+
+	private JobBuilderFactory jobBuilderFactory;
+	private StepBuilderFactory stepBuilderFactory;
+
+	public BatchConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+		this.jobBuilderFactory = jobBuilderFactory;
+		this.stepBuilderFactory = stepBuilderFactory;
+	}
+
+	@Bean
+	public Job job() {
+		return this.jobBuilderFactory.get("job")
+				.start(step1())
+				.build();
+	}
+
+	@Bean
+	public Step step1() {
+		return this.stepBuilderFactory.get("step1")
+				.tasklet((contribution, context) -> {
+					System.out.println(">> Tasklet was run");
+					return RepeatStatus.FINISHED;
+				})
+				.build();
+	}
+}
