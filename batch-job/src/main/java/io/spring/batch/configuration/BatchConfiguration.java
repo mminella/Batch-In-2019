246c99167001ede793c786b22b84a6091ea86750
Michael Minella <mminella@pivotal.io>
2019-03-03 11:51:47 -0600

    Updates

--- batch-job/src/main/java/io/spring/batch/configuration/BatchConfiguration.java
+++ batch-job/src/main/java/io/spring/batch/configuration/BatchConfiguration.java
@@ -47,6 +47,7 @@ public class BatchConfiguration {
 	public Job job() {
 		return this.jobBuilderFactory.get("job")
 				.start(step1())
+				.next(step2())
 				.incrementer(new RunIdIncrementer())
 				.build();
 	}
@@ -56,6 +57,16 @@ public class BatchConfiguration {
 		return this.stepBuilderFactory.get("step1")
 				.tasklet((contribution, context) -> {
 					System.out.println(">> Tasklet was run");
+					return RepeatStatus.FINISHED;
+				})
+				.build();
+	}
+
+	@Bean
+	public Step step2() {
+		return this.stepBuilderFactory.get("step2")
+				.tasklet((contribution, context) -> {
+					System.out.println(">> Tasklet was run, again!");
 					return RepeatStatus.FINISHED;
 				})
 				.build();
