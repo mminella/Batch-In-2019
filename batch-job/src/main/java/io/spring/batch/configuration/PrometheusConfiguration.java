725c366738e3825ccc2f759023e3b1c96d06313e
Michael Minella <mminella@pivotal.io>
2019-10-01 12:58:39 -0500

    Removed explicit prom push gateway configuration code

--- batch-job/src/main/java/io/spring/batch/configuration/PrometheusConfiguration.java
+++ batch-job/src/main/java/io/spring/batch/configuration/PrometheusConfiguration.java
@@ -15,19 +15,7 @@
  */
 package io.spring.batch.configuration;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.time.Duration;
-import java.util.Map;
-
-import io.prometheus.client.CollectorRegistry;
-import io.prometheus.client.exporter.PushGateway;
-
-import org.springframework.boot.actuate.autoconfigure.metrics.export.prometheus.PrometheusProperties;
-import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusPushGatewayManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 /**
  * @author Michael Minella
@@ -40,26 +28,26 @@ public class PrometheusConfiguration {
 //		return new LoggingMeterRegistry();
 //	}
 
-	@Bean
-	public PrometheusPushGatewayManager prometheusPushGatewayManager(
-			CollectorRegistry collectorRegistry,
-			PrometheusProperties prometheusProperties, Environment environment) throws MalformedURLException {
-		PrometheusProperties.Pushgateway properties = prometheusProperties
-				.getPushgateway();
-		PushGateway pushGateway = new PushGateway(new URL(properties.getBaseUrl()));
-		Duration pushRate = properties.getPushRate();
-		String job = getJob(properties, environment);
-		Map<String, String> groupingKey = properties.getGroupingKey();
-		PrometheusPushGatewayManager.ShutdownOperation shutdownOperation = properties.getShutdownOperation();
-		return new PrometheusPushGatewayManager(pushGateway, collectorRegistry,
-				pushRate, job, groupingKey, shutdownOperation);
-	}
-
-	private String getJob(PrometheusProperties.Pushgateway properties,
-			Environment environment) {
-		String job = properties.getJob();
-		job = (job != null) ? job
-				: environment.getProperty("spring.application.name");
-		return (job != null) ? job : "spring";
-	}
+//	@Bean
+//	public PrometheusPushGatewayManager prometheusPushGatewayManager(
+//			CollectorRegistry collectorRegistry,
+//			PrometheusProperties prometheusProperties, Environment environment) throws MalformedURLException {
+//		PrometheusProperties.Pushgateway properties = prometheusProperties
+//				.getPushgateway();
+//		PushGateway pushGateway = new PushGateway(new URL(properties.getBaseUrl()));
+//		Duration pushRate = properties.getPushRate();
+//		String job = getJob(properties, environment);
+//		Map<String, String> groupingKey = properties.getGroupingKey();
+//		PrometheusPushGatewayManager.ShutdownOperation shutdownOperation = properties.getShutdownOperation();
+//		return new PrometheusPushGatewayManager(pushGateway, collectorRegistry,
+//				pushRate, job, groupingKey, shutdownOperation);
+//	}
+//
+//	private String getJob(PrometheusProperties.Pushgateway properties,
+//			Environment environment) {
+//		String job = properties.getJob();
+//		job = (job != null) ? job
+//				: environment.getProperty("spring.application.name");
+//		return (job != null) ? job : "spring";
+//	}
 }
