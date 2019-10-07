781df4e4a5160bb42a70794c1ce9a6202f975f4f
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2019-10-07 11:28:03 -0500

    Rollback new feature (test CI/CD)

--- batch-job/src/main/java/io/spring/batch/configuration/JobConfiguration.java
+++ batch-job/src/main/java/io/spring/batch/configuration/JobConfiguration.java
@@ -19,19 +19,15 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.listener.StepExecutionListenerSupport;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.support.ListItemReader;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -56,26 +52,17 @@ public class JobConfiguration {
 	@Bean
 	public Job job() {
 		return jobBuilderFactory.get("job")
-				.start(step1())
-				.next(step2())
+				.start(step())
 				.incrementer(new RunIdIncrementer())
 				.build();
 	}
 
 	@Bean
-	public Step step1() {
-		return stepBuilderFactory.get("step1")
+	public Step step() {
+		return stepBuilderFactory.get("step")
 				.<Integer, Integer>chunk(3)
 				.reader(itemReader())
 				.writer(itemWriter())
-				.listener(new StepExecutionListenerSupport() {
-					@Override
-					public ExitStatus afterStep(StepExecution stepExecution) {
-						stepExecution.getJobExecution().getExecutionContext()
-								.put("readCount", stepExecution.getReadCount());
-						return super.afterStep(stepExecution);
-					}
-				})
 				.build();
 	}
 
@@ -103,17 +90,6 @@ public class JobConfiguration {
 				System.out.println("item = " + item);
 			}
 		};
-	}
-
-	@Bean
-	public Step step2() {
-		return stepBuilderFactory.get("step2")
-				.tasklet((contribution, chunkContext) -> {
-					System.out.println("read count = " + chunkContext
-							.getStepContext().getJobExecutionContext().get("readCount"));
-					return RepeatStatus.FINISHED;
-				})
-				.build();
 	}
 
 }
