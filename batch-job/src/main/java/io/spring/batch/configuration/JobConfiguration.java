3421df07f68da4b47640838b8922fb93a82b7d98
Michael Minella <mminella@pivotal.io>
2019-10-07 22:14:50 -0500

    Here we go again

--- batch-job/src/main/java/io/spring/batch/configuration/JobConfiguration.java
+++ batch-job/src/main/java/io/spring/batch/configuration/JobConfiguration.java
@@ -21,17 +21,13 @@ import java.util.Random;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.support.ListItemReader;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -57,21 +53,8 @@ public class JobConfiguration {
 	public Job job() {
 		return jobBuilderFactory.get("job")
 				.start(step())
-				.next(step2())
 				.incrementer(new RunIdIncrementer())
 				.build();
-	}
-
-	@Bean
-	public Step step2() {
-		return this.stepBuilderFactory.get("step2")
-				.tasklet(new Tasklet() {
-					@Override
-					public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-						System.out.println("STEP 2 RAN!!!");
-						return RepeatStatus.FINISHED;
-					}
-				}).build();
 	}
 
 	@Bean
